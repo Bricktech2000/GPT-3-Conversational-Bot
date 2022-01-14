6f5e8e0905a4b35ccaf98abd092808a3910eac4d
Bricktech <bricktech2000@gmail.com>
2022-01-13 22:39:11 -0500

    code cleanup

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
-from hashlib import new
-from re import S
 import discord
 import openai
 import json
-import time
 
-from conversation import Conversation
+# import time
+# from conversation import Conversation
 
-config = json.load(open("config.json"))
-
-client = discord.Client()
 
 conversations = []
+current_chats = []
 
+config = json.load(open("config.json"))
+bot_sequence = "\nBot:"
+training_data = open("session_prompt.txt", "r").read()
+client = discord.Client()
 openai.api_key = config['openai_api_key']
 
-bot_sequence = "\nBot:"
-session_prompt = open("session_prompt.txt", "r").read()
 NUM_CHATS = 10
-current_chats = []
 
 def ask(question, username_sequence, chat_log=None):
     prompt_text = f'{chat_log}{username_sequence} {question}\n'
-    print(prompt_text, end='')
+    print('\n\n\n\n', prompt_text, end='')
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt_text,
         temperature=0.8,
-        max_tokens=150,
+        max_tokens=75,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
@@ -44,31 +41,28 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global current_chats
-    if message.author.bot: return
 
-    convo = None
+    # if message.author.bot: return
 
-    for i in conversations:
-        if time.time() - i.timeAlive > 5000:
-            conversations.pop(i)
-            continue
+    # convo = None
 
-        if message.channel == i.channel:
-            convo = i
+    # for i in conversations:
+    #     if time.time() - i.timeAlive > 5000:
+    #         conversations.pop(i)
+    #         continue
+    #     if message.channel == i.channel:
+    #         convo = i
     
-    if convo == None:
-        convo = Conversation(message.author, message.channel, None)
-
+    # if convo == None:
+    #     convo = Conversation(message.author, message.channel, None)
 
     username = message.author.display_name
     username_sequence = f'\n{username}:'
 
     current_chats.append(f'{username_sequence} {message.content}')
-    print('\n\n\n\n\n', current_chats)
-    response = ask(message.content, username_sequence, session_prompt + ''.join(current_chats[-NUM_CHATS:-1]))
+    response = ask(message.content, username_sequence, training_data + ''.join(current_chats[-NUM_CHATS:-1]))
 
     if response and "Bot: " in response:
-        # session_prompt += f'{bot_sequence} {response}'
         response = response.replace("Bot: ", "")
         current_chats.append(f'{bot_sequence} {response}')
         await message.channel.send(response)
