1711cda1844941c3efea7157fc81ba3a59595565
Bricktech <bricktech2000@gmail.com>
2022-01-13 21:30:50 -0500

    made some changes

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ config = json.load(open("config.json"))
 client = discord.Client()
 
 conversations = []
+usernames = []
 
 openai.api_key = config['openai_api_key']
 
-start_sequence = "\nBOT:"
-restart_sequence = "\n\nPerson:"
+start_sequence = "\nBot:"
+restart_sequence = "\nPerson1:"
 session_prompt = open("session_prompt.txt", "r").read()
 
 def ask(question, chat_log=None):
-    prompt_text = f'{chat_log}{restart_sequence} {question}{start_sequence}'
-    prompt_text = prompt_text.replace("BOT", session_prompt.split('\n')[0])
-    print(prompt_text)
+    prompt_text = f'{chat_log}{restart_sequence} {question}\n'
+    print(prompt_text, end='')
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt_text,
@@ -30,11 +30,11 @@ def ask(question, chat_log=None):
         max_tokens=150,
         top_p=1,
         frequency_penalty=0,
-        presence_penalty=0.3,
+        presence_penalty=0.6,
         stop=["\n"],
     )
     story = response['choices'][0]['text']
-    # print(f'response: str(story)')
+    print(f'{str(story)}')
     return str(story)
 
 @client.event
@@ -61,8 +61,11 @@ async def on_message(message):
 
     response = ask(message.content, session_prompt)
 
-    if response:
-        session_prompt = f'{session_prompt}{restart_sequence} {message.content}{start_sequence}{response} '
+    session_prompt += f'{restart_sequence} {message.content}'
+    if response and "Bot: " in response:
+        # print("BOT: " in response)
+        response = response.replace("Bot: ", "")
+        session_prompt += f'{start_sequence} {response}'
         await message.channel.send(response)
 
     # no need for this
